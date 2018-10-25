d5796ce3a910e6abf0d908327cc0dbcd83e2afd1
TinQk <potie.quentin@gmail.com>
2018-10-25 10:50:40 +0200

    oups j'ai oublié de push le seed !

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+#10.times do |index|
+#  user = User.create!(first_name: "fname#{index}", last_name: Faker::GameOfThrones.house, email: Faker::Internet.email)
+#end
+
+#5.times do |index|
+#  category = Category.create!(name: Faker::Food.dish)
+#end
+
+
+
+
+
+
+
+
+
+10.times do |index|
+  article
+
+
+
+
+#  article = Article.create!(title: Faker::Simpsons.quote, content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit #amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?")
+#end
+
+
+
+#15.times do |index|
+#  comment = Comment.create!(content: Faker::DrWho.quote)
+#end
